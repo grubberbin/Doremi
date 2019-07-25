6bcce1551f0d2468fb64e509c6d0bc1a22ed0f3f
徐彬彬 <xubinbin@xubinbins-MBP.lan>
2019-07-25 10:54:42 +0800

    Revert "增加xadmin 路由"
    This reverts commit 3e8d71866cb5b243facff96a4b8bbbd167d2a78b.

--- Doremi/urls.py
+++ Doremi/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import xadmin
 from . import view
 
 urlpatterns = [
-    path('xadmin/', xadmin.site.urls),
+    path('admin/', admin.site.urls),
     path('index/', view.index)
 ]
