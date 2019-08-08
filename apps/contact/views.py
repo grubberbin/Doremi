c82def597b15f81f072d891af3cc47e7a3d79acf
griy <griy26@163.com>
2019-08-08 12:35:12 +0800

    留言功能提交

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from django.http import HttpResponse
 
 from .forms import ContactForm
 from .models import ContactInfo
 
-import datetime
+import json
 
 
 # Create your views here.
@@ -17,18 +18,19 @@ class ContactView(View):
 
     def post(self, request):
         contact_form = ContactForm(request.POST)
+        res = dict()
         if contact_form.is_valid():
             username = request.POST.get('username', None)
             phone = request.POST.get('phone', None)
             message = request.POST.get('message', None)
-            if username and phone and message:
-                info = ContactInfo()
-                info.username = username
-                info.mobile = phone
-                info.message = message
-                info.save()
-                return render(request, 'contact.html', {'msg': '提交成功！'})
-            else:
-                return render(request, 'contact.html', {'msg': '数据错误'})
+            info = ContactInfo()
+            info.username = username
+            info.mobile = phone
+            info.message = message
+            info.save()
+            res['status'] = 'success'
+            res['msg'] = '留言提交成功！'
         else:
-            return render(request, 'contact.html', {'msg': '手机号格式错误'})
+            res['status'] = 'fail'
+            res['msg'] = '格式错误'
+        return HttpResponse(json.dumps(res), content_type='application/json')
