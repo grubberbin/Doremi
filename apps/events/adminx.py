2edef29b78dbdd0fbbe4bc5778b73d93b44c7b44
griy <griy26@163.com>
2019-09-06 11:21:15 +0800

    活动应用提交

---
+++ apps/events/adminx.py
@@ -0,0 +1,24 @@
+import xadmin
+from xadmin import views
+from .models import Events
+
+
+# ----- adminx 全局配置
+class BaseSetting:
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings:
+    site_title = '哆唻咪后台管理系统'
+    site_footer = '哆唻咪启蒙乐园 2019-2099'
+    menu_style = 'accordion'
+
+
+class EventsManager(object):
+    list_display = ['id', 'title', 'create_time']
+    search_fields = ['title']
+    list_filter = []
+
+
+xadmin.site.register(Events, EventsManager)
--- apps/events/models.py
+++ apps/events/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Events(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, verbose_name='标题', null=False, blank=False)
+    price = models.CharField(max_length=50, verbose_name='费用', null=False, blank=False)
+    create_time = models.DateTimeField(null=False, blank=False, verbose_name='发布时间')
+    start_time = models.DateTimeField(null=False, blank=False, verbose_name='开始时间')
+    end_time = models.DateTimeField(null=False, blank=False, verbose_name='结束时间')
+    address = models.CharField(max_length=50, verbose_name='地点', null=False, blank=False)
+    awards = models.CharField(max_length=50, verbose_name='奖项', null=False, blank=False)
+    class_id = models.CharField(max_length=50, verbose_name='活动对象', null=False, blank=False)
+
+    class Meta:
+        verbose_name = '活动'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
