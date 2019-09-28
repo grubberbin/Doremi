502a439bb896ba4c1fa0d5b74e3b44ecb901c9b5
griy <griy26@163.com>
2019-09-28 11:42:07 +0800

    商城路由和字段更新

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 
-from .views import ShopInfoView, AddCartView, ProductInfoView, CheckoutPageView, CartView
+from .views import ShopInfoView, AddCartView, ProductInfoView, CheckoutPageView, CartInfoView, updateCartView
 
 app_name = 'shop'
 
 urlpatterns = [
     # 商城信息
     path('', ShopInfoView.as_view(), name='shop'),
-    path('cart/', CartView.as_view(), name='cart'),
+    path('cart/', CartInfoView.as_view(), name='cart'),
     path('cart/<int:goods_id>/<int:quantity>', AddCartView.as_view(), name='add_cart'),
     path('cart/<int:goods_id>/<int:quantity>', AddCartView.as_view(), name='delete_cart'),
-    path('cart/<int:goods_id>/<int:quantity>', AddCartView.as_view(), name='update_cart'),
+    path('cart/update', updateCartView.as_view(), name='update_cart'),
     path('order/', CheckoutPageView.as_view(), name='order'),
     path('goods/<int:goods_id>/', ProductInfoView.as_view(), name='good_details'),
     path('details/', ProductInfoView.as_view(), name='details'),
--- apps/shop/views.py
+++ apps/shop/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from django.db.models import Q
 from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Goods, Order, Cart
+from users.models import UserProfile
 from django.views.decorators.csrf import csrf_exempt
+from utils.mixin_utils import LoginRequiredMixin
 import json
 
 
 # Create your views here.
 
 
-class ShopInfoView(View):
+class ShopInfoView(LoginRequiredMixin, View):
 
     def get(self, request):
         goods_list = Goods.objects.all()
@@ -28,10 +31,16 @@ class ProductInfoView(View):
         return render(request, 'shop/product-details.html', {'goods': goods, 'others_list': others_list})
 
 
-class CartView(View):
+class CartInfoView(LoginRequiredMixin, View):
 
     def get(self, request):
-        cart_list = Cart.objects.all()
+
+        username = request.user
+        user = UserProfile.objects.get(username=username)
+        cart_list = Cart.objects.filter(u_id=user)
+        for cart in cart_list:
+            cart.total = float(cart.g_id.price) * int(cart.count)
+
         return render(request, 'shop/cart-page.html', {'cart_list': cart_list})
 
     @csrf_exempt
@@ -67,6 +76,45 @@ class AddCartView(View):
         else:
             msg = '欢迎购物！'
         return render(request, 'shop/cart-page.html', {'goods_list': goods_list, 'msg': msg})
+
+
+class updateCartView(View):
+    def post(self, request):
+        user = request.user
+        if not user.is_authenticated:
+            return JsonResponse({'res': 0, 'errmsg': '请先登录'})
+
+        # 接收数据
+        sku_id = request.POST.get('sku_id')
+        count = request.POST.get('count')
+
+        # 数据校验
+        if not all([sku_id, count]):
+            return JsonResponse({'res': 1, 'errmsg': '数据不完整'})
+
+        # 校验添加的商品数：
+        try:
+            count = int(count)
+        except Exception as e:
+            # 数目出错
+            return JsonResponse({'res': 2, 'errmsg': '商品数目出错'})
+
+        # 校验商品是否存在：
+        try:
+            sku = Goods.objects.get(id=sku_id)
+        except Goods.DoesNotExist:
+            # 商品不存在
+            return JsonResponse({'res': 3, 'errmsg': '商品不存在'})
+
+        # 校验商品库存
+        if count > sku.stock:
+            return JsonResponse({'res': 4, 'errmsg': '商品库存不足'})
+
+        # 业务处理：更新购物车
+        Cart.objects.filter(g_id=sku).update(count=count)
+
+        # 返回应答
+        return JsonResponse({'res': 5, 'message': '更新成功'})
 
 
 class CheckoutPageView(View):
